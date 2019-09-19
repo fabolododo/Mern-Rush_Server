134a7c48d41f27c35f9e485a173a2cda5b7b7b7b
fabolododo <fabolododo@hotmail.fr>
2019-09-19 19:34:00 +0200

    comment production env static

--- server.js
+++ server.js
@@ -17,14 +17,14 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 // Test route
 app.get('/', (req, res) => {
@@ -35,6 +35,6 @@ app.get('/test', (req,res) => {
   res.send("<h1>This is Working</h1>")
 })
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
