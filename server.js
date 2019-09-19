8370ae2553c7d3a99b755f513774d7db4b56bc8a
fabolododo <fabolododo@hotmail.fr>
2019-09-19 19:19:37 +0200

    added Test route

--- server.js
+++ server.js
@@ -26,6 +26,11 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+// Test route
+app.use('/', (req, res) => {
+  res.send('API is running')
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
