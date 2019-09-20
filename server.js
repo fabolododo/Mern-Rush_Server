64f5eed5148a070692c487b232a2d23010b97057
fabolododo <fabolododo@hotmail.fr>
2019-09-20 12:34:30 +0200

    added cors and body-parser

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
-
+// const cors = require('cors');
+// const bodyParser = require("body-parser");
 const app = express();
 
 // Connect Database
@@ -9,6 +10,24 @@ connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var allowedOrigins = ['http://localhost:8080','https://104.198.14.52', 'http://localhost:3000'];
+app.use(cors({
+    origin: function(origin, callback) {
+        if (!origin)
+            return callback(null, true);
+
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not allow acces from specified origin.';
+            return(callback(new Error(msg), false));
+        }
+
+        return(callback(null, true));
+    }
+}));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -18,7 +37,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
 // if (process.env.NODE_ENV === 'production') {
-//   // Set static folder
+  // Set static folder
 //   app.use(express.static('client/build'));
 
 //   app.get('*', (req, res) => {
